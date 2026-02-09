f4628495edcca10fb7e3ec430e08a897b1bdd137
kaif475869-creator <kaif475869@gmail.com>
2026-02-09 12:46:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+const CACHE_NAME = 'pwice-v1';
+const assets = ['/', '/index.html', '/manifest.json', '/favicon.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response("Offline mode active");
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
-
